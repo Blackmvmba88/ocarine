b32edce18bffbf16d7353f79610e18344af7181e
BlackMamba <neocyber1@gmail.com>
2026-08-22 03:36:38 -0600

    style: load scoring UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './controlProfiles.css'
 import './practice.css'
+import './scoring.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
